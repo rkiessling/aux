fa595b5435c63a38dfe55b1166262d0d58ecc817
rk1973unsl <74510712+rk1973unsl@users.noreply.github.com>
2021-09-21 17:34:16 -0300

    Update funaux.py

--- funaux.py
+++ funaux.py
@@ -1,13 +1,3 @@
-from __future__ import print_function
-print("importando modulos de ploteo, audio e interaccion")
-
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython.display import Audio
-from scipy.io import wavfile
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
-
 print("definiendo funciones auxiliares")
 
 def normalizar(x):
