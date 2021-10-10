92ab6c1eb4b2ed1f756d0c77d75e1a3868b5d28b
rk1973unsl <74510712+rk1973unsl@users.noreply.github.com>
2021-10-09 23:44:30 -0300

    Update funaux.py

--- funaux.py
+++ funaux.py
@@ -205,29 +205,6 @@ def xtriang(fs,periodo, tmax=1, tipo='t', polaridad='b'):
     x=normalizarb(x)
   return x,t
 
-def xtriang(fs,periodo, tmax=1, tipo='t', polaridad='b'):
-  '''
-  señal triangular o diente de  sierra:
-  primer argumento, frecuencia de muestreo
-  segundo argumento, periodo del diente de sierra
-  tercer argumento, tiempo de simulacion
-  cuarto argumento, tipo: 't'riangular /\/\/\, 'c'reciente  /|/|/|, 'd'ecreciente  |\|\|\
-  quinto argumento, polaridad: 'u'nipolar (0,1), 'b'ipolar (-1,1)
-  entrega x, t: valores de la funcion, valores de tiempo 
-  '''
-  t = np.arange(0, tmax, step=1. / fs)
-  if tipo=='c':
-    x = np.mod(t,periodo)
-  elif tipo=='d':
-    x = periodo-np.mod(t,periodo)
-  elif tipo=='t':
-    x = 1-np.abs(periodo/2-np.mod(t,periodo))
-  if polaridad=='u':
-    x=normalizar(x)
-  else:
-    x=normalizarb(x)
-  return x,t
-
 def xpulsos(fs, periodo, ciclo, tmax=1, polaridad='b'):
   '''
   señal pulsos
